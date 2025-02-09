402c2619c72d96a98949db098c067273a96aa346
Goarik Mkrtchyan <mkrtchyangoarik@gmail.com>
2025-02-09 17:11:13 +0300

    3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# 1SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '89.169.174.201', 'gogoshkafoodgram.zapto.org']
