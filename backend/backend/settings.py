b2d18f17e17f5c7923b0ca41ddbd0f546a9b3ab0
Goarik Mkrtchyan <mkrtchyangoarik@gmail.com>
2025-03-02 14:16:50 +0300

    33

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '89.169.174.201', 'gogoshkafoodgram.zapto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
